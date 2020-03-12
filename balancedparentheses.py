1eb224d9006973c39a5ffe5ca8a3498cc7e256d4
Janine Martinez <vagrant@vagrant.vm>
2020-03-12 22:09:10 +0000

    balanced parentheses

---
+++ balancedparentheses.py
@@ -0,0 +1,54 @@
+"""Does a string have balanced parentheses?
+
+For example::
+
+   >>> has_balanced_parens("()")
+   True
+
+   >>> has_balanced_parens("(Oh Noes!)(")
+   False
+
+   >>> has_balanced_parens("((There's a bonus open paren here.)")
+   False
+
+   >>> has_balanced_parens(")")
+   False
+
+   >>> has_balanced_parens("(")
+   False
+
+   >>> has_balanced_parens("(This has (too many closes.) ) )")
+   False
+
+If you receive a string with no parentheses, consider it balanced::
+
+   >>> has_balanced_parens("Hey...there are no parens here!")
+   True
+"""
+
+
+def has_balanced_parens(phrase):
+    """Does a string have balanced parentheses?"""
+
+
+    string_open = 0
+    string_close = 0
+
+    for i in phrase:
+      if i == ")":
+        string_close += 1
+      elif i == "(":
+        string_open += 1
+      else:
+        continue
+
+    if string_open == string_close:
+      return True
+    else:
+      return False
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU CAUGHT ALL THE STRAY PARENS!\n")
