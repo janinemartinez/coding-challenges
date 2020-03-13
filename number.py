236d522ac39a2e732c998d1105445eae78d5e195
Janine Martinez <vagrant@vagrant.vm>
2020-03-13 20:23:29 +0000

    code_challenge 3

---
+++ number.py
@@ -0,0 +1,24 @@
+
+def missing_number(nums, max_num):
+    """Given a list of numbers 1...max_num, find which one is missing.
+
+    *nums*: list of numbers 1..[max_num]; exactly one digit will be missing.
+    *max_num*: Largest potential number in list
+
+    >>> missing_number([7, 3, 2, 4, 5, 6, 1, 9, 11, 10, 21, 89], 10)
+    8
+    
+    """
+
+    return sum(range(max_num+1))-sum([x for x in nums if x <= max_num])
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASS. NICELY DONE!\n")
+
+
+
+
+    
